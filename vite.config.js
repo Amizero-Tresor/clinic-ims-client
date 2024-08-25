f534a991f8f1cae3540f8810db8abbcf64f83c51
Amizero-Tresor <peaceamizero@gmail.com>
2024-08-25 23:08:19 +0200

    [feat]: solved scrollbar error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import postcssImport from 'postcss-import';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [postcssImport()],
+    },
+  },
+});
